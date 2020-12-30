11988303c1c8049882e66f8c925fcdedae3cab41
Edward <muhedwardc@gmail.com>
2020-12-30 20:24:25 +0700

    interface

--- src/interface.ts
+++ src/interface.ts
@@ -12,4 +12,21 @@ person1 = {
     greet: (phrase: string) => phrase
 }
 
-// person1.greet('Hello world!');
+// person1.greet('Hello world!'); 
+
+interface FullName {
+    firstName: string;
+    lastName: string;
+    getFullName(phrase: string): string;
+}
+
+class PersonClass implements PersonInterface {
+    constructor(public name: string, public age: number, public greet: (phrase: string) => string) {}
+
+    greetPerson(this: PersonClass, prefix: string) {
+        return `${prefix} ${this.greet(this.name)}`
+    }
+}
+
+const person1Class = new PersonClass('Edward', 22, (phrase: string) => phrase);
+console.log(person1Class.greetPerson('Hello'));
