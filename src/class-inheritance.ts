8f8621a37bd0b86947382048fce5d7a043195b15
Edward <muhedwardc@gmail.com>
2020-12-30 17:10:47 +0700

    abstract class

--- src/class-inheritance.ts
+++ src/class-inheritance.ts
@@ -1,10 +1,10 @@
-class Department {
+abstract class Department {
     static hello = 'HELLO';
     // private readonly id: string;
     // private name: string;
     protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
+    constructor(protected readonly id: string, public name: string) {
         // this.id = id;
         // this.name = n;
 
@@ -17,9 +17,7 @@ class Department {
         return { name: name };
     }
 
-    describe(this: Department) {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
+    abstract describe(this: Department): void;
 
     addEmployee(employee: string) {
         // validation etc
@@ -38,6 +36,11 @@ class ITDepartment extends Department {
     constructor(id: string, admins: string[]) {
         super(id, 'IT');
         this.admins = admins;
+    }
+
+    // should added because its abstract
+    describe(this: ITDepartment) {
+        console.log(`IT: ${this.id}`);
     }
 }
 
@@ -77,6 +80,12 @@ class AccountingDepartment extends Department {
 
     printReports() {
         console.log(this.reports);
+    }
+
+
+    // should added because its abstract
+    describe(this: AccountingDepartment) {
+        console.log(`ACcounting: ${this.id}`);
     }
 }
 
