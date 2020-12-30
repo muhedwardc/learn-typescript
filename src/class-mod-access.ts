f2aea09ed7f777d8be169b02e3c427c8a48ab2f3
Edward <muhedwardc@gmail.com>
2020-12-30 16:20:32 +0700

    class mod access

---
+++ src/class-mod-access.ts
@@ -0,0 +1,28 @@
+class Person {
+    firstName: string;
+    public /* default*/ lastName: string;
+    private hobbies: string[] = [];
+
+    constructor(f: string, l: string) {
+        this.firstName = f;
+        this.lastName = l;
+    }
+
+    fullName(this: User /* this should refer to User */): string {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    addHobby(hobby: string) {
+        this.hobbies.push(hobby)
+    }
+
+    getHobbiesStr() {
+        return this.hobbies.join(', ');
+    }
+}
+
+const newPerson = new Person('Edward', 'Chakim');
+newPerson.addHobby('running (i wish)');
+newPerson.addHobby('eating');
+newPerson.getHobbiesStr(); // return 'running (i wish), eating'
+// newPerson.hobbies; --> cannot access, it's private!!
