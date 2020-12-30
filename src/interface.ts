4e63ad6aa55edd04b1fa782035b97ea319a44612
Edward <muhedwardc@gmail.com>
2020-12-30 20:36:58 +0700

    extend interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,4 +1,13 @@
-interface PersonInterface {
+enum GenderEnum { 
+    male = 'male', 
+    female ='female' 
+};
+
+interface GenderInterface {
+    readonly gender: GenderEnum
+}
+
+interface PersonInterface extends GenderInterface {
     name: string;
     age: number;
     greet(phrase: string): string;
@@ -9,6 +18,7 @@ let person1: PersonInterface;
 person1 = {
     name: 'Edward',
     age: 22,
+    gender: GenderEnum.male,
     greet: (phrase: string) => phrase
 }
 
@@ -20,13 +30,13 @@ interface FullName {
     getFullName(phrase: string): string;
 }
 
-class PersonClass implements PersonInterface {
-    constructor(public name: string, public age: number, public greet: (phrase: string) => string) {}
+class PersonClass implements PersonInterface, GenderInterface {
+    constructor(public name: string, public age: number, public gender: GenderEnum, public greet: (phrase: string) => string) {}
 
     greetPerson(this: PersonClass, prefix: string) {
         return `${prefix} ${this.greet(this.name)}`
     }
 }
 
-const person1Class = new PersonClass('Edward', 22, (phrase: string) => phrase);
+const person1Class = new PersonClass('Edward', 22, GenderEnum.male, (phrase: string) => phrase);
 console.log(person1Class.greetPerson('Hello'));
