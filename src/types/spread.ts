15f537fd12d2575b2c7777285dab8da756360175
Edward <muhedwardc@gmail.com>
2020-12-30 13:53:40 +0700

    rest params

--- src/types/spread.ts
+++ src/types/spread.ts
@@ -11,3 +11,9 @@ const person = {
 // const copiedPerson = person;
 const copiedPerson = { ...person };
 
+const addNumbers = (...numbers: number[]): number => {
+    return numbers.reduce((res, val) => res + val, 0);
+}
+
+const addedNumbers = addNumbers(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(addedNumbers);
