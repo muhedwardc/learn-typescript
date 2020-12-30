cf69bee69482151eecbf25f7935a126a59c1c5cb
Edward <muhedwardc@gmail.com>
2020-12-30 16:46:05 +0700

    get set

--- src/class-inheritance.ts
+++ src/class-inheritance.ts
@@ -1,5 +1,7 @@
 class Department {
-    private employees: string[] = [];
+    // private readonly id: string;
+    // private name: string;
+    protected employees: string[] = [];
   
     constructor(private readonly id: string, public name: string) {
       // this.id = id;
@@ -31,12 +33,37 @@ class Department {
   }
   
   class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[] = []) {
+    private lastReport: string;
+  
+    get mostRecentReport() {
+      if (this.lastReport) {
+        return this.lastReport;
+      }
+      throw new Error('No report found.');
+    }
+  
+    set mostRecentReport(value: string) {
+      if (!value) {
+        throw new Error('Please pass in a valid value!');
+      }
+      this.addReport(value);
+    }
+  
+    constructor(id: string, private reports: string[]) {
       super(id, 'Accounting');
+      this.lastReport = reports[0];
+    }
+  
+    addEmployee(name: string) {
+      if (name === 'Ed') {
+        return;
+      }
+      this.employees.push(name);
     }
   
     addReport(text: string) {
       this.reports.push(text);
+      this.lastReport = text;
     }
   
     printReports() {
@@ -59,9 +86,15 @@ class Department {
   
   const accounting = new AccountingDepartment('d2', []);
   
+  accounting.mostRecentReport = 'Year End Report';
   accounting.addReport('Something went wrong...');
+  console.log(accounting.mostRecentReport);
+  
+  accounting.addEmployee('Ed');
+  accounting.addEmployee('Ward');
   
   accounting.printReports();
+  accounting.printEmployeeInformation();
   
   // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
   
