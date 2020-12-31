af9b789f1f0b39d817c1f8c6a95c6a1a1c9fc383
Edward <muhedwardc@gmail.com>
2020-12-31 15:03:37 +0700

    accessor & parameter decorator

--- src/decorator.ts
+++ src/decorator.ts
@@ -35,11 +35,27 @@ function Log(target: any, propertyName: string | Symbol) {
     console.log({ target, propertyName })
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator!')
+    console.log({ target, name, descriptor })
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator!')
+    console.log({ target, name, descriptor })
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!')
+    console.log({ target, name, position })
+}
+
 class Product {
     @Log
     title: string
     private _price: number
 
+    @Log2
     set price(val: number) {
         if (val > 0) {
             this._price = val;
@@ -53,7 +69,8 @@ class Product {
         this._price = p;
     }
 
-    getPriceWithTax(tax: number) {
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + tax);
     }
 }
