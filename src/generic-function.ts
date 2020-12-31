53c759b1b2bf57d839a470c28de740f2880d109f
Edward <muhedwardc@gmail.com>
2020-12-31 13:23:29 +0700

    generic with interface

--- src/generic-function.ts
+++ src/generic-function.ts
@@ -13,3 +13,21 @@ const mergedData = merge(
 console.log(mergedData.name);
 
 // const mergedDataInvalid = merge({name: 'Edward'}, 20); // passing number as non object will fail
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDesc<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'No value!';
+    if (element.length === 1) {
+        descriptionText = `Got 1 element.`;
+    } else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements.`;
+    }
+    return [element, descriptionText];
+}
+
+// console.log(countAndDesc(10)) // number has no length prop
+console.log(countAndDesc('Hi!'));
+console.log(countAndDesc(['Test', 'Bos']));
