4d61ce036e75de67b8a7ce7d2877566a06812c17
Edward <muhedwardc@gmail.com>
2020-12-31 14:52:15 +0700

    more decorator

--- src/decorator.ts
+++ src/decorator.ts
@@ -5,7 +5,20 @@ function Logger(logString: string) {
     }
 }
 
-@Logger('LOGGING - PET')
+function WithTemplate(template: string, hookId: string) {
+    return function(constructor: any) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
+// @Logger('LOGGING - PET')
+@WithTemplate('<h1>Pet class</h1>', 'app')
 class Pet {
     type = 'cat';
     name = 'katty';
